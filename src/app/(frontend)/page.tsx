c808e0aef7318c670ea8c9bf13fad1a73ec12c5c
kourdroid <kourdroid@gmail.com>
2025-12-25 23:15:22 +0100

    Update page.tsx

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,6 +1,27 @@
-import PageTemplate, { generateMetadata } from './[slug]/page'
+import React from 'react'
+import { generateMetadata } from './[slug]/page'
+import { Hero } from '@/components/landing/Hero'
+import { TrustedBy } from '@/components/landing/TrustedBy'
+import { Intro } from '@/components/landing/Intro'
+import { Ecosystem } from '@/components/landing/Ecosystem'
+import { Domains } from '@/components/landing/Domains'
+import { Journal } from '@/components/landing/Journal'
+import { WhyChoose } from '@/components/landing/WhyChoose'
+import { CTA } from '@/components/landing/CTA'
 
-export default PageTemplate
+export default function Page() {
+  return (
+    <main className="min-h-screen  mx-auto w-full font-sans bg-smatch-black text-smatch-text-primary selection:bg-smatch-gold selection:text-smatch-black">
+      <Hero />
+      <TrustedBy />
+      <Intro />
+      <Ecosystem />
+      <Domains />
+      <Journal />
+      <WhyChoose />
+      <CTA />
+    </main>
+  )
+}
 
 export { generateMetadata }
-
