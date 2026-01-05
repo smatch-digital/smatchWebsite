1bd788480c7fe9f0c33372e35f12d70ce69a1ec9
kourdroid <kourdroid@gmail.com>
2026-01-06 00:11:06 +0100

    authorize maps

--- next.config.js
+++ next.config.js
@@ -18,6 +18,10 @@ const nextConfig = {
         protocol: 'http',
         hostname: '**',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
     ],
   },
   webpack: (webpackConfig) => {
