97f5a79c1e77ddf3851f1ef553cdac132805b8de
kourdroid <kourdroid@gmail.com>
2026-02-21 17:45:17 +0000

    chore: clean up project and update gitignore

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '*.supabase.co',
       },
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
       // Production domain
       {
         protocol: 'https',
